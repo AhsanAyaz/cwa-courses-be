523fadf3a0efc497661549ea9fa4f9a297c8916c
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2022-03-23 03:34:55 +0100

    added youtube videos for existing courses

--- src/api/course/controllers/course.js
+++ src/api/course/controllers/course.js
@@ -6,4 +6,15 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::course.course');
+module.exports = createCoreController("api::course.course", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+    if (!query.filters) query.filters = {};
+    query.filters.slug = { $eq: slug };
+    const entity = await strapi.service("api::course.course").find(query);
+    const { results } = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(results[0]);
+  },
+}));
