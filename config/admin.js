040e0c9e96bdf33a8c36fb690a12378f35a59796
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-01-12 04:26:55 +0100

    chore(update): update to strapi 4.5.6
    also updated the course and chapter relations

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'e4350524b0689fc3f46e2407dae950e2'),
+    secret: env("ADMIN_JWT_SECRET", "e4350524b0689fc3f46e2407dae950e2"),
+  },
+  apiToken: {
+    salt: env("ADMIN_TOKEN_SALT", "aWK6KR4EmbWrwTO+HWhoaw=="),
   },
 });
