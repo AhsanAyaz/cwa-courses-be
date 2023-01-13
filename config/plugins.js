a1b27b6a53da304a17625815d983206961065a98
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-01-13 02:58:29 +0100

    feat(vercel-deploy): add vercel deployment hook

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,25 @@
 module.exports = ({ env }) => ({
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-  });
+  },
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook: env("VERCEL_DEPLOY_HOOK"),
+      apiToken: env("VERCEL_API_TOKEN"),
+      appFilter: "code-with-ahsan",
+      roles: ["strapi-super-admin"],
+    },
+  },
+});
