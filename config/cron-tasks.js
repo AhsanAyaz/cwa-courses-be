eb6024cd3cd9010cf86bd644ec51e23aa64714e5
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2023-06-03 23:15:59 +0200

    chore(cron): disable cronjob

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -1,16 +1,16 @@
-const { ytToStrapi } = require("../scripts/posts-cron");
+// const { ytToStrapi } = require("../scripts/posts-cron");
 
 module.exports = {
   uploadYTVids: {
     task: ({ strapi }) => {
       /* Add your own logic here */
-      console.log('cron started at ', new Date());
-      ytToStrapi().then(() => {
-        console.log('cron finished at ', new Date());
-      }).catch(err => {
-        console.log('cron failed at ', new Date());
-        console.log(err);
-      });
+      // console.log('cron started at ', new Date());
+      // ytToStrapi().then(() => {
+      //   console.log('cron finished at ', new Date());
+      // }).catch(err => {
+      //   console.log('cron failed at ', new Date());
+      //   console.log(err);
+      // });
     },
     options: {
       rule: "17 15 * * 0-6"
