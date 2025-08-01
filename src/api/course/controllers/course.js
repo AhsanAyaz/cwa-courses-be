624ae989e9c16b2f659d179da6264f1f32d79e74
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2025-08-01 17:11:56 +0200

    feat: Add order to chapters and sort them in course view
    This commit cherry-picks the changes from the feature/chapter-ordering branch that do not modify environment variables.
    
    - Added an 'order' field to the chapter schema.
    - Modified the course controller to populate chapters and sort them by the new 'order' field.

--- src/api/course/controllers/course.js
+++ src/api/course/controllers/course.js
@@ -12,6 +12,21 @@ module.exports = createCoreController("api::course.course", ({ strapi }) => ({
     const { query } = ctx;
     if (!query.filters) query.filters = {};
     query.filters.slug = { $eq: slug };
+
+    const chaptersPopulate = {
+      chapters: {
+        sort: ["order:asc"],
+      },
+    };
+
+    if (Array.isArray(query.populate)) {
+      const newPopulate = {};
+      query.populate.forEach((field) => (newPopulate[field] = true));
+      query.populate = { ...newPopulate, ...chaptersPopulate };
+    } else {
+      query.populate = { ...query.populate, ...chaptersPopulate };
+    }
+
     const entity = await strapi.service("api::course.course").find(query);
     const { results } = await this.sanitizeOutput(entity, ctx);
 
